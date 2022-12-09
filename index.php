89dd2233a016d0073e8e0619c6003d4d841c8f3c
xingkongmcqwq <118539719+xingkongmcqwq@users.noreply.github.com>
2022-12-09 14:17:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <title>欢迎来到星空的主站</title>
+</head>
+<body>
+<link rel="stylesheet" href="css/zz.css">
+<section>
+    <div id="one">
+        <div id="two">
+            <div id="font">
+                <div class="color"></div>
+                <div class="color"></div>
+                <div class="color"></div>
+                <div class="box">
+                    <div class="circle" style="--x:0"></div>
+                    <div class="circle" style="--x:1"></div>
+                    <div class="circle" style="--x:2"></div>
+                    <div class="circle" style="--x:3"></div>
+                    <div class="circle" style="--x:4"></div>
+                    <h1>欢迎来到星空的主站</h1>
+                    
+                    <h3>星空主站：
+                        <a id="two" href="https://xingkongserver.top" >https://xingkongserver.top</h3>
+                    <h3>星空二级域名分发:
+                        <a id="two" href="https://name.xingkongserver.top">https://name.xingkongserver.top</h3></a>
+                    <h3>星空博客站：
+                        <a id="two" href="https://blog.xingkongserver.top">https://blog.xingkongserver.top</h3></a>
+                    <h3>星空哔哩哔哩：
+                        <a id="two" href="https://space.bilibili.com/158191454" >https://space.bilibili.com/158191454</h3></a>
+                    <h3>星空爱发电：
+                        <a id="two" href="https://afdian.net/a/xingkonga">https://afdian.net/a/xingkonga</h3></a>
+                    <iframe src="https://afdian.net/leaflet?slug=xingkonga" width="640" scrolling="no" height="200" frameborder="0"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">Copyright©2022 starry sky. All Rights Reserved.</div>
+</section>
+
+</body>
+</html>
